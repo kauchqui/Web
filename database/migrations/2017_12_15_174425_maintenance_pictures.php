b0a5959b1dd15418505a10dade7617d579cdbc22
kauchqui <quintinkauchick@yahoo.com>
2017-12-31 16:26:23 -0500

    added picture relation

---
+++ database/migrations/2017_12_15_174425_maintenance_pictures.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MaintenancePictures extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintenancepictures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('unit_id');
+            $table->string('picture');
+            $table->integer('maintenance_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop(maintenancepictures);
+    }
+}
