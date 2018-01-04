9e7219f4b424a3e18cf93a0c04bec0a850dc333f
kauchqui <quintinkauchick@yahoo.com>
2018-01-04 01:05:29 -0500

    Updated about page with email functionality

---
+++ resources/views/mail.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body style="background: black; color: white">
+<h1>Contact Us Inquiry:</h1>
+<p>Name: {{$first}} {{$last}}</p>
+<p>Phone: {{$phone}}</p>
+<p>Email: {{$email}}</p>
+<p>Message: {{$content}}</p>
+</body>
+</html>
